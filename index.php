0f330e6d2a91ae0a3090dabf44183f46370b5acc
Mirza <mirza.delagic@gmail.com>
2019-06-11 13:51:43 +0000

    i to je početak

---
+++ index.php
@@ -0,0 +1,39 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+
+  <title>The HTML5 Herald</title>
+  <meta name="description" content="The HTML5 Herald">
+  <meta name="author" content="SitePoint">
+
+  <link rel="stylesheet" href="stil.css">
+  
+  <style>
+  .plavo{
+	color: rgb(0,0,200);
+  }
+  </style>
+
+</head>
+
+<body>
+	<h1>Hello PP19 ekipa</h1>
+	<p class="plavo">ovo je paragraf</p>
+	<div>
+		<p id="vazno">ovo je drugi paragraf</p>
+	</div>
+	<h1 class="plavo" style="color: blue">Drugi</h1>
+	
+	
+	<?php 
+	
+	phpinfo();
+	
+	?>
+	
+	
+  <script src="skripta.js"></script>
+</body>
+</html>
---
+++ skripta.js
@@ -0,0 +1,3 @@
+alert("Hello");
+document.write("Hello");
+console.log("Hello");
